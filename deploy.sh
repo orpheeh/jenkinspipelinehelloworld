86d0c1b78360a32f45ce53d97414a5f0c87bdd8f
orpheeh <39984314+orpheeh@users.noreply.github.com>
2024-01-11 10:01:22 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-sudo git checkout origin/master
+#!/bin/bash
+
+cd /home/orphee/jenkinspipelinehelloworld 
+
+sudo git checkout master
+sudo git pull origin master
+
 sudo docker compose down
 sudo docker compose up --build -d
